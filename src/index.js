8d3333586bd61de56374a2cd6053d96d7d4a1fef
Andy <andy@widgetic.com>
2020-10-30 15:10:26 +0200

    Index.js: add startPlaying event and play public function

--- src/index.js
+++ src/index.js
@@ -8,12 +8,12 @@ export default class Widget extends WidgetBase {
 
   // widget's api public functions
   getPublicMethods() {
-    return ['publicFunction1', 'publicFunction2'];
+    return ['publicFunction1', 'publicFunction2', 'play'];
   }
 
   // widget's api public events
   getPublicEvents() {
-    return ['event1', 'event2', 'event3'];
+    return ['event1', 'event2', 'startPlaying'];
   }
 }
 
@@ -27,5 +27,5 @@ var modules = {};
 modules[WIDGET_NAME] = function(exports, require, module) {
   module.exports = Widget;
 };
-
+// console.log("WIDGET MODULES:", modules);
 window.require.define(modules);
